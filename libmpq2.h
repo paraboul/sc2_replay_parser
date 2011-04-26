ee8368390de3d71ceca8d4a69d0469fe0cbd3250
paraboul <a.catel@weelya.com>
2011-04-27 00:15:00 +0200

    WIP

--- libmpq2.h
+++ libmpq2.h
@@ -7,11 +7,49 @@
 
 #define MPQ_MAGIC 0x1B51504D
 
+typedef struct _sc2string {
+    char *val;
+    unsigned int length;
+} sc2string;
+
 typedef struct _mpqsc2
 {
     mpq_archive_s *mpq;
     
 } MPQSC2;
+
+typedef enum {
+    PS_READ_START,
+    PS_READ_SHORTNAME_LENGTH,
+    PS_READ_SHORTNAME
+    
+} SC2_PLAYERS_DETAILS_STATE;
+
+typedef struct _sc2_player_defails
+{
+    sc2string short_name;
+    sc2string full_name;
+    sc2string race;
+    
+    struct _sc2_player_defails *next;
+    
+} SC2_PLAYERS_DETAILS;
+
+typedef struct _sc2_replay_details
+{
+    uint8_t nplayers;
+    
+    char *map_name;
+    char *minimap_filename;
+    char *map_path;
+    
+    SC2_PLAYERS_DETAILS *players;
+} SC2_REPLAY_DETAILS;
+
+#ifndef MIN
+    #define MIN(val1, val2) ((val1 > val2) ? (val2) : (val1))
+    #define MAX(val1, val2) ((val1 < val2) ? (val2) : (val1))
+#endif
 
 #endif
 
--- main.c
+++ main.c
@@ -23,7 +23,90 @@ MPQSC2 *libmpq_sc2_init(const char *filename)
     
 }
 
-unsigned char *libmpq_sc2_readfile(MPQSC2 *scr, const char *filename)
+uint64_t _libmpq_sc2_parse_player_details(unsigned char *data, uint64_t size,
+    SC2_PLAYERS_DETAILS **player)
+{
+    uint64_t pos;
+    SC2_PLAYERS_DETAILS_STATE state = PS_READ_START;
+    
+    *player = malloc(sizeof(SC2_PLAYERS_DETAILS));
+    (*player)->short_name.val = NULL;
+    (*player)->full_name.val = NULL;
+    (*player)->race.val = NULL;
+    
+    for (pos = 0; pos < size; pos++) {
+        switch(state) {
+            case PS_READ_START:
+                if (pos == 3) {
+                    state = PS_READ_SHORTNAME_LENGTH;
+                }
+                break;
+            case PS_READ_SHORTNAME_LENGTH:
+                (*player)->short_name.length = (uint8_t)data[pos] / 2;
+                if ((*player)->short_name.length > 64) {
+                    goto error;
+                }
+                (*player)->short_name.val = malloc((*player)->short_name.length);
+                state = PS_READ_SHORTNAME;
+                break;
+            case PS_READ_SHORTNAME:
+                break;
+            default:
+                break;
+        }
+    }
+    
+    return pos;
+    
+error:
+    free((*player)->short_name.val);
+    free((*player)->full_name.val);
+    free((*player)->race.val);
+    
+    free(*player);
+    
+    return 0;
+}
+
+SC2_REPLAY_DETAILS *_libmpq_sc2_parse_replay_details(unsigned char *data, 
+    uint64_t size)
+{
+    #define CHECK_OVERFLOW() if (pos > size) return NULL
+    uint64_t pos;
+    
+    SC2_REPLAY_DETAILS *details = malloc(sizeof(*details));
+    
+    details->nplayers           = 0;
+    details->map_name           = NULL;
+    details->minimap_filename   = NULL;
+    details->map_path           = NULL;
+    
+    for (pos = 0; pos < size; pos++) {
+        switch(pos) {
+            case 6:
+                details->nplayers = (uint8_t)data[pos] / 2;
+                break;
+            case 7:
+            {
+                uint8_t pstruct;
+
+                for (pstruct = 0; pstruct < 16; pstruct++) {
+                    SC2_PLAYERS_DETAILS *player;
+                    
+                    pos += _libmpq_sc2_parse_player_details(&data[pos], 
+                                size-pos, &player);
+                                
+                    CHECK_OVERFLOW();
+                }
+            }
+            break;
+        }
+    }
+    
+    return details;
+}
+
+unsigned char *libmpq_sc2_readfile(MPQSC2 *scr, const char *filename, libmpq__off_t *size)
 {
     uint32_t filenumber = 0;
     libmpq__off_t filesize = 0, transferred = 0;
@@ -45,23 +128,29 @@ unsigned char *libmpq_sc2_readfile(MPQSC2 *scr, const char *filename)
         return NULL;
     }
     
+    *size = transferred;
+    
     return read;
 }
 
 int main(int argc, char **argv)
 {
     MPQSC2 *scr;
+    unsigned char *content;
+    int64_t size = 0;
     
-    if ((scr = libmpq_sc2_init("meta.SC2Replay")) == NULL) {
+    if ((scr = libmpq_sc2_init("1v1.sc2replay")) == NULL) {
         printf("Init failed\n");
         return 0;
     }
 
-    if (libmpq_sc2_readfile(scr, "replay.details") == NULL) {
+    if ((content = libmpq_sc2_readfile(scr, "replay.details", &size)) == NULL) {
         printf("Failed to read subfile\n");
-        return;
+        return 0;
     }
     
+    _libmpq_sc2_parse_replay_details(content, size);
 
 	return 1;
 }
+
